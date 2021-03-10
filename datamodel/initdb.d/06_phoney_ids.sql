b40e5d8a0416bbcb1376ac7d26b0ec4e616fbace
Mason Leon <44509637+masonleon@users.noreply.github.com>
2021-03-09 20:32:29 -0500

    remove unnecessary tables

--- datamodel/initdb.d/06_phoney_ids.sql
+++ datamodel/initdb.d/06_phoney_ids.sql
@@ -1,26 +1,28 @@
--- Assumes the PSQL client
-\set ON_ERROR_STOP true
-\connect dcsa_openapi
+/*
+    -- Assumes the PSQL client
+    \set ON_ERROR_STOP true
+    \connect dcsa_openapi
 
--- Currently an implementation detail for the reference implementation but one
--- we would like to remove eventually.
-BEGIN;
+    -- Currently an implementation detail for the reference implementation but one
+    -- we would like to remove eventually.
+    BEGIN;
 
--- Most of the R2DBC tooling we are currently using requires that every entity
--- has a direct ID.  This change is to insert these, so the code works but they
--- are not a part of the original data model.
+    -- Most of the R2DBC tooling we are currently using requires that every entity
+    -- has a direct ID.  This change is to insert these, so the code works but they
+    -- are not a part of the original data model.
 
-ALTER TABLE dcsa_ebl_v1_0.document_party
-    ADD COLUMN IF NOT EXISTS id uuid DEFAULT uuid_generate_v4() PRIMARY KEY;
+    ALTER TABLE dcsa_ebl_v1_0.document_party
+        ADD COLUMN IF NOT EXISTS id uuid DEFAULT uuid_generate_v4() PRIMARY KEY;
 
-ALTER TABLE dcsa_ebl_v1_0.displayed_address
-    ADD COLUMN IF NOT EXISTS id uuid DEFAULT uuid_generate_v4() PRIMARY KEY;
+    ALTER TABLE dcsa_ebl_v1_0.displayed_address
+        ADD COLUMN IF NOT EXISTS id uuid DEFAULT uuid_generate_v4() PRIMARY KEY;
 
 
-ALTER TABLE dcsa_ebl_v1_0.shipment_location
-    ADD COLUMN IF NOT EXISTS id uuid DEFAULT uuid_generate_v4() PRIMARY KEY;
+    ALTER TABLE dcsa_ebl_v1_0.shipment_location
+        ADD COLUMN IF NOT EXISTS id uuid DEFAULT uuid_generate_v4() PRIMARY KEY;
 
-ALTER TABLE dcsa_ebl_v1_0.cargo_line_item
-    ADD COLUMN IF NOT EXISTS id uuid DEFAULT uuid_generate_v4() PRIMARY KEY;
+    ALTER TABLE dcsa_ebl_v1_0.cargo_line_item
+        ADD COLUMN IF NOT EXISTS id uuid DEFAULT uuid_generate_v4() PRIMARY KEY;
 
-COMMIT;
+    COMMIT;
+*/
