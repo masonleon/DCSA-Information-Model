0e5da353f411779ea0a967d632cf855a9b348939
Mason Leon <44509637+masonleon@users.noreply.github.com>
2021-03-09 21:12:52 -0500

    remove unneeded

--- datamodel/initdb.d/06_phoney_ids.sql
+++ datamodel/initdb.d/06_phoney_ids.sql
@@ -1,28 +1,27 @@
-/*
-    -- Assumes the PSQL client
-    \set ON_ERROR_STOP true
-    \connect dcsa_openapi
 
-    -- Currently an implementation detail for the reference implementation but one
-    -- we would like to remove eventually.
-    BEGIN;
+-- -- Assumes the PSQL client
+-- \set ON_ERROR_STOP true
+-- \connect dcsa_openapi
 
-    -- Most of the R2DBC tooling we are currently using requires that every entity
-    -- has a direct ID.  This change is to insert these, so the code works but they
-    -- are not a part of the original data model.
+-- -- Currently an implementation detail for the reference implementation but one
+-- -- we would like to remove eventually.
+-- BEGIN;
 
-    ALTER TABLE dcsa_ebl_v1_0.document_party
-        ADD COLUMN IF NOT EXISTS id uuid DEFAULT uuid_generate_v4() PRIMARY KEY;
+-- -- Most of the R2DBC tooling we are currently using requires that every entity
+-- -- has a direct ID.  This change is to insert these, so the code works but they
+-- -- are not a part of the original data model.
 
-    ALTER TABLE dcsa_ebl_v1_0.displayed_address
-        ADD COLUMN IF NOT EXISTS id uuid DEFAULT uuid_generate_v4() PRIMARY KEY;
+-- ALTER TABLE dcsa_ebl_v1_0.document_party
+--     ADD COLUMN IF NOT EXISTS id uuid DEFAULT uuid_generate_v4() PRIMARY KEY;
+
+-- ALTER TABLE dcsa_ebl_v1_0.displayed_address
+--     ADD COLUMN IF NOT EXISTS id uuid DEFAULT uuid_generate_v4() PRIMARY KEY;
 
 
-    ALTER TABLE dcsa_ebl_v1_0.shipment_location
-        ADD COLUMN IF NOT EXISTS id uuid DEFAULT uuid_generate_v4() PRIMARY KEY;
+-- ALTER TABLE dcsa_ebl_v1_0.shipment_location
+--     ADD COLUMN IF NOT EXISTS id uuid DEFAULT uuid_generate_v4() PRIMARY KEY;
 
-    ALTER TABLE dcsa_ebl_v1_0.cargo_line_item
-        ADD COLUMN IF NOT EXISTS id uuid DEFAULT uuid_generate_v4() PRIMARY KEY;
+-- ALTER TABLE dcsa_ebl_v1_0.cargo_line_item
+--     ADD COLUMN IF NOT EXISTS id uuid DEFAULT uuid_generate_v4() PRIMARY KEY;
 
-    COMMIT;
-*/
+-- COMMIT;
